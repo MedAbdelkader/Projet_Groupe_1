ec79686d8934d451ef8a8dc83497ba12e31c7de6
SamirAffes <samir.affes@esprit.tn>
2025-01-15 19:12:43 +0100

    ajouter un attribut telephone

--- src/entites/Client.java
+++ src/entites/Client.java
@@ -8,6 +8,7 @@ public class Client {
     private String email;
     private List<Commande> commandes;
     private String prenom;
+    private int telephone ;
 
     // Constructeur paramétré
     public Client(String nom, String email) {
