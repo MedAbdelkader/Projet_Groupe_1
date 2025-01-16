2bd075a4571d4d84521fa21de3bd1f6575471e80
louaybenslama <louay.benslama@esprit.tn>
2025-01-16 17:55:26 +0100

    coding classe commande 2

--- src/entites/Commande.java
+++ src/entites/Commande.java
@@ -1,4 +1,45 @@
 package entites;
 
 public class Commande {
+    private int idCommande;
+    private double montantTotal;
+    private Client client;
+    private String status;
+
+    // Constructeur paramétré
+    public Commande(int idCommande, double montantTotal, Client client) {
+        this.idCommande = idCommande;
+        this.montantTotal = montantTotal;
+        this.client = client;
+        this.status = "En attente";
+    }
+
+    // Méthode pour afficher les détails de la commande
+    public void afficherDetails() {
+        System.out.println("ID Commande: " + idCommande + ", Montant Total: " + montantTotal +
+                ", Client: " + client.getNom() + ", Statut: " + status);
+    }
+
+    // Méthode pour modifier le statut de la commande
+    public void modifierStatut(String nouveauStatut) {
+        this.status = nouveauStatut;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Commande{" +
+                "idCommande=" + idCommande +
+                ", montantTotal=" + montantTotal +
+                ", client=" + client +
+                ", status='" + status + '\'' +
+                '}';
+    }
+
+    // Méthode pour obtenir le montant total de la commande
+    public double getMontantTotal() {
+        return montantTotal;
+
+
+
+    }
 }
