e0151fe8921cca1008506b113951915017361e0e
HoucineSaihi <houcinesaihi6@gmail.com>
2025-01-17 21:09:05 +0100

    changed some attribute names

--- src/entites/Depot.java
+++ src/entites/Depot.java
@@ -2,11 +2,11 @@ package entites;
 
 public class Depot {
 
-    private int quantiteDisponible;
-    private Produit produit;
+    private int quantity;
+    private Produit product;
 
     private int id;
-    private float salaire;
+    private float salary;
 
 
 }
