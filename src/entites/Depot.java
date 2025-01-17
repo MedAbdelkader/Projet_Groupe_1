23b03f6696da5d75e1abd7321e9ec458ec9f282a
HoucineSaihi <houcinesaihi6@gmail.com>
2025-01-17 21:24:55 +0100

    added supplier to depot

--- src/entites/Depot.java
+++ src/entites/Depot.java
@@ -2,8 +2,10 @@ package entites;
 
 public class Depot {
 
+
     private int quantity_of_products;
     private Produit product;
+    private Supplier supplier;
 
     private int id;
     private float salary;
